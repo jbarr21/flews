01af407beafeef4499797178ca18cdc3a18e7630
James Barr <barr.james.r@gmail.com>
2018-04-15 13:43:55 -0700

    Update dependencies

--- lib/imgur/api.dart
+++ lib/imgur/api.dart
@@ -6,14 +6,14 @@ import 'package:flews/imgur/imgur.dart';
 import 'package:flews/serializers.dart';
 import 'package:flews/util.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 const String baseUrl = 'https://api.imgur.com/3/';
 const String mostViralUrl = 'gallery/hot/viral?showMature=true';
 const String clientId = 'cc86a8de0e7c459';
 
 Future<List<ImageItem>> getMostViral() async {
-  final httpClient = createHttpClient();
-  final response = await httpClient.get(Url.baseUrl(baseUrl, mostViralUrl),
+  final response = await http.get(Url.baseUrl(baseUrl, mostViralUrl),
       headers: {'authorization': 'Client-ID $clientId'});
 
   GalleryItems photos = serializers.deserializeWith(
--- lib/reddit/api.dart
+++ lib/reddit/api.dart
@@ -5,13 +5,13 @@ import 'package:flews/reddit/post.dart';
 import 'package:flews/serializers.dart';
 import 'package:flews/util.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 const String baseUrl = 'https://www.reddit.com/r/AndroidDev/';
 const String topPostsUrl = 'top.json?count=25';
 
 Future<List<Post>> getTopPosts() async {
-  final httpClient = createHttpClient();
-  final response = await httpClient.get(Url.baseUrl(baseUrl, topPostsUrl));
+  final response = await http.get(Url.baseUrl(baseUrl, topPostsUrl));
 
   Posts posts =
       serializers.deserializeWith(Posts.serializer, JSON.decode(response.body));
